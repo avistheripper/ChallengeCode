65819830e8be041c7567d54e6bfd7c191a53c54a
avistheripper <bboyxns@mail.ru>
2019-05-02 11:08:23 +0300

    Logout added

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private loginService: LoginService) { }
+  onLogout(): void {
+    this.loginService.userLogout();
   }
 
 }
