9f811ca3bade836388edebd5f5e206392c55a647
avistheripper <bboyxns@mail.ru>
2019-05-04 16:33:40 +0300

    Login navigate after logout

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -8,9 +9,13 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class NavComponent {
 
-  constructor(private loginService: LoginService) { }
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+    ) { }
   onLogout(): void {
     this.loginService.userLogout();
+    this.router.navigate(['/login']);
   }
 
 }
